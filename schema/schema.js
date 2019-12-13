9e6ac740cd171862d01d1baa817745cc4f5048c3
Naoto Yamaguchi <naotoyamaguchi@users.noreply.github.com>
2019-12-13 00:40:11 -0800

    feat: added initial root query

--- schema/schema.js
+++ schema/schema.js
@@ -13,3 +13,17 @@ const UserType = new GraphQLObjectType({
     age: { type: GraphQLInt }
   }
 });
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        // hit DB / external api; get data
+
+      }
+    }
+  }
+});
