99cc341a04628648e08c860d8719dee8ef5ced30
Naoto Yamaguchi <naotoyamaguchi@users.noreply.github.com>
2019-12-13 01:02:40 -0800

    feat: added initial root query to middleware schema option

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 // Using express middleware
 app.use('/graphql', expressGraphQL({
+  // flag for development only
+  schema,
   graphiql: true
 }));
 
