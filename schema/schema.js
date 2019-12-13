618feb27405b0733269ffb8e82cc1130ac4f5bb6
Naoto Yamaguchi <naotoyamaguchi@users.noreply.github.com>
2019-12-13 00:47:08 -0800

    feat: added mock data

--- schema/schema.js
+++ schema/schema.js
@@ -1,9 +1,15 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt
 } = graphql;
+
+const users = [
+  { id: '23', firstName: 'Nao', age: 27 },
+  { id: '47', firstName: 'Agnes', age: 28 }
+]
 
 const UserType = new GraphQLObjectType({
   name: 'User',
